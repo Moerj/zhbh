fc1d11e849463bdc6b35015a26ceb5f32a85cd30
cloydlau <*>
2020-08-24 17:48:21 +0800

    Create index.vue

---
+++ src/components/Topbar/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <van-icon name="arrow-left" v-if="!$route.meta.root"/>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .van-icon {
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+}
+</style>
