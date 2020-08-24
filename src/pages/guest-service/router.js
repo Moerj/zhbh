e70fec3ce56f8bf01a9740b48ae7b9390c0b03e6
cloydlau <*>
2020-08-24 10:57:33 +0800

    stashed

--- src/pages/guest-service/router.js
+++
@@ -1,7 +0,0 @@
-export default [
-  {
-    path: '/guest-service',
-    name: '嘉宾服务',
-    component: () => import('./index.vue'),
-  },
-]
--- src/pages/sign-in/router.js
+++
@@ -1,7 +0,0 @@
-export default [
-  {
-    path: 'sign-in',
-    name: '签到',
-    component: () => import('./index.vue'),
-  }
-]
